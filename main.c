ded5763f3242c562503dc41fdc22f65c68a294b0
toasa <atsushi.tohyama.160.333@gmail.com>
2019-02-21 20:29:24 +0900

    add: permission

--- main.c
+++ main.c
@@ -8,8 +8,41 @@
 void print_time(time_t *t) {
     struct tm tm;
     localtime_r(t, &tm);
-    printf("%04d %2d %02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+    printf("%2d %02d", tm.tm_mon + 1, tm.tm_mday);
     printf(" %02d:%02d", tm.tm_hour, tm.tm_min);
+}
+
+void print_perm(int p, char c) {
+    if (p) {
+        printf("%c", c);
+    } else {
+        printf("-");
+    }
+}
+
+void print_type_perm(mode_t mt) {
+    if (mt & S_IFDIR) {
+        printf("d");
+    } else if (mt & S_IFREG) {
+        printf("-");
+    } else if (mt & S_IFLNK) {
+        printf("l");
+    } else if (mt & S_IFBLK) {
+        printf("b");
+    } else if (mt & S_IFCHR) {
+        printf("c");
+    }
+
+    print_perm(mt & S_IRUSR, 'r');
+    print_perm(mt & S_IWUSR, 'w');
+    print_perm(mt & S_IXUSR, 'x');
+    print_perm(mt & S_IRGRP, 'r');
+    print_perm(mt & S_IWGRP, 'w');
+    print_perm(mt & S_IXGRP, 'x');
+    print_perm(mt & S_IROTH, 'r');
+    print_perm(mt & S_IWOTH, 'w');
+    print_perm(mt & S_IXOTH, 'x');
+
 }
 
 void print_stat(char *path) {
@@ -17,18 +50,11 @@ void print_stat(char *path) {
     stat(path, &st_buf);
     // printf("st_dev: %d\n", st_buf.st_dev);
     // printf("st_ino: %llu\n", st_buf.st_ino);
-    // printf("st_mode: %d\n", st_buf.st_mode);
-    // printf("st_nlink: %d\n", st_buf.st_nlink);
-    // printf("st_uid: %d\n", st_buf.st_uid);
-    // printf("st_gid: %d\n", st_buf.st_gid);
-
-    // printf("st_size: %lld\n", st_buf.st_size);
-    printf("  %5lld  ", st_buf.st_size);
-
-    // printf("st_blksize: %d\n", st_buf.st_blksize);
-    // printf("st_blocks: %lld\n", st_buf.st_blocks);
-
-    //printf("st_mtime: %ld\n", st_buf.st_mtime);
+    print_type_perm(st_buf.st_mode);
+    printf("%3d", st_buf.st_nlink);
+    printf(" %5d", st_buf.st_uid);
+    printf("     %5d", st_buf.st_gid);
+    printf(" %5lld ", st_buf.st_size);
     print_time(&st_buf.st_mtime);
 }
 
