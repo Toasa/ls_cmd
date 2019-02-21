23bbe401fad4d4190968a53d9cfed9bd13055d4c
toasa <atsushi.tohyama.160.333@gmail.com>
2019-02-21 19:12:27 +0900

    add: skip  and  directory

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
@@ -10,11 +11,11 @@ void print_stat(char *path) {
     printf("st_ino: %llu\n", st_buf.st_ino);
     printf("st_mode: %d\n", st_buf.st_mode);
     printf("st_nlink: %d\n", st_buf.st_nlink);
-    printf("st_uid: %d\n", st_buf.st_uid);
-    printf("st_gid: %d\n", st_buf.st_gid);
+    // printf("st_uid: %d\n", st_buf.st_uid);
+    // printf("st_gid: %d\n", st_buf.st_gid);
     printf("st_size: %lld\n", st_buf.st_size);
-    printf("st_blksize: %d\n", st_buf.st_blksize);
-    printf("st_blocks: %lld\n", st_buf.st_blocks);
+    // printf("st_blksize: %d\n", st_buf.st_blksize);
+    // printf("st_blocks: %lld\n", st_buf.st_blocks);
 }
 
 void ls(char *dname) {
@@ -26,6 +27,9 @@ void ls(char *dname) {
         printf("err\n");
     } else {
 	    while ((direntp = readdir(dir_ptr)) != NULL) {
+            if (!(strcmp(direntp->d_name, ".") && strcmp(direntp->d_name, "..")))
+                continue;
+
             printf("%s\n", direntp->d_name);
             print_stat(direntp->d_name);
         }
